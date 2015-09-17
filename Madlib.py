b232b8fbc11860211ba84796f24a30ec22ad90f8
skipamos <skipamos@gmail.com>
2015-09-17 10:32:55 -0700

    changed story

--- Madlib.py
+++ Madlib.py
@@ -1,1 +1,13 @@
-Hello
+name = input("Please enter a man's name ")
+madlib = name
+madlib += " is a normal "
+
+madlib += input("Enter an occupation")
+madlib += " learning how to program at code guild.  Then, one day, Thunder made a new ."
+madlib += input("Enter a noun ")
+madlib += " .  When {} tried to download the new repo his ".format(name)
+madlib += input("Enter a noun ")
+madlib += " exploded in the shape of a fiery "
+madlib += input("shape ")
+madlib += " and {} was never heard from again.".format(name)
+print(madlib)
